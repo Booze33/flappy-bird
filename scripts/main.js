258a419b5ca31421e1f4fe117a4fcfda6598890e
Booze33 <tebetisloh@gmail.com>
2024-07-02 11:03:33 +0200

    Set up project

--- scripts/main.js
+++ scripts/main.js
@@ -1,122 +1,133 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const canvas = document.getElementById('gameCanvas');
-  const ctx = canvas.getContext('2d');
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-
-  const game = new Game(canvas, ctx);
-  game.start();
-});
-
 class Game {
   constructor(canvas, ctx) {
     this.canvas = canvas;
     this.ctx = ctx;
-    this.width = canvas.width;
-    this.height = canvas.height;
-    this.player = new Player(this);
-    this.gravity = 0.5;
-    this.animationFrameId = null;
-  }
-
-  start() {
-    this.animate();
-    window.addEventListener('resize', () => this.resizeCanvas());
-  }
-
-  animate() {
-    this.ctx.clearRect(0, 0, this.width, this.height);
-    this.player.update();
-    this.player.draw();
-    this.animationFrameId = requestAnimationFrame(() => this.animate());
-  }
-
-  resizeCanvas() {
-    this.canvas.width = window.innerWidth;
-    this.canvas.height = window.innerHeight;
     this.width = this.canvas.width;
     this.height = this.canvas.height;
+    this.player = new Player(this);
+    this.baseHeight = 720;
+    this.ratio = this.height / this.baseHeight;
+    // this.gravity = 0.5;
+    // this.animationFrameId = null;
+
+    this.resize(window.innerWidth, window.innerHeight);
+
+    window.addEventListener('resize', (e) => {
+      this.resize(window.innerWidth, window.innerHeight);
+    })
+  }
+
+  resize(width, height) {
+    this.canvas.width = width;
+    this.canvas.height = height;
+    this.ctx.fillStyle = 'red';
+    this.width = this.canvas.width;
+    this.height = this.canvas.height;
+    this.ratio = this.height / this.baseHeight;
+
+    this.player.resize();
+  }
+
+  render() {
+    this.player.update();
+    this.player.draw();
   }
 }
 
-class Obstacle {
-  constructor(game) {
-    this.game = game;
-    this.width = 50;
-    this.height = Math.random() * (this.game.height / 2);
-    this.x = this.game.width;
-    this.y = Math.random() > 0.5 ? 0 : this.game.height - this.height;
-    this.speedX = 5;
+window.addEventListener('load', () => {
+  const canvas = document.getElementById('gameCanvas');
+  const ctx = canvas.getContext('2d');
+  canvas.width = 720;
+  canvas.height = 720;
+
+  const game = new Game(canvas, ctx);
+
+  const animate = () => {
+    ctx.clearReact(0, 0, canvas.width, canvas.height)
+    game.render();
+    requestAnimationFrame(animate);
   }
+  requestAnimationFrame(animate);
+})
 
-  update() {
-    this.x -= this.speedX;
-  }
+// class Obstacle {
+//   constructor(game) {
+//     this.game = game;
+//     this.width = 50;
+//     this.height = Math.random() * (this.game.height / 2);
+//     this.x = this.game.width;
+//     this.y = Math.random() > 0.5 ? 0 : this.game.height - this.height;
+//     this.speedX = 5;
+//   }
 
-  draw() {
-    this.game.ctx.fillStyle = 'blue';
-    this.game.ctx.fillRect(this.x, this.y, this.width, this.height);
-  }
-}
+//   update() {
+//     this.x -= this.speedX;
+//   }
 
-Game.prototype.spawnObstacle = function() {
-  if (Math.random() < 0.02) {
-      this.obstacles.push(new Obstacle(this));
-  }
-};
+//   draw() {
+//     this.game.ctx.fillStyle = 'blue';
+//     this.game.ctx.fillRect(this.x, this.y, this.width, this.height);
+//   }
+// }
 
-Game.prototype.animate = function() {
-  this.ctx.clearRect(0, 0, this.width, this.height);
-  this.player.update();
-  this.player.draw();
-  this.obstacles.forEach(obstacle => {
-    obstacle.update();
-    obstacle.draw();
-  });
-  this.spawnObstacle();
-  this.animationFrameId = requestAnimationFrame(() => this.animate());
-};
+// Game.prototype.spawnObstacle = function() {
+//   if (Math.random() < 0.02) {
+//       this.obstacles.push(new Obstacle(this));
+//   }
+// };
 
-Game.prototype.resizeCanvas = function() {
-  this.canvas.width = window.innerWidth;
-  this.canvas.height = window.innerHeight;
-  this.width = this.canvas.width;
-  this.height = this.canvas.height;
-  this.ctx.imageSmoothingEnabled = false;
-};
+// Game.prototype.animate = function() {
+//   this.ctx.clearRect(0, 0, this.width, this.height);
+//   this.player.update();
+//   this.player.draw();
+//   this.obstacles.forEach(obstacle => {
+//     obstacle.update();
+//     obstacle.draw();
+//   });
+//   this.spawnObstacle();
+//   this.animationFrameId = requestAnimationFrame(() => this.animate());
+// };
 
-Game.prototype.animate = function() {
-  this.ctx.clearRect(0, 0, this.width, this.height);
-  this.player.update();
-  this.player.draw();
+// Game.prototype.resizeCanvas = function() {
+//   this.canvas.width = window.innerWidth;
+//   this.canvas.height = window.innerHeight;
+//   this.width = this.canvas.width;
+//   this.height = this.canvas.height;
+//   this.ctx.imageSmoothingEnabled = false;
+// };
 
-  this.obstacles.forEach((obstacle, index) => {
-      obstacle.update();
-      obstacle.draw();
-      if (this.player.collidesWith(obstacle)) {
-          cancelAnimationFrame(this.animationFrameId);
-          alert('Game Over');
-      }
-  });
-  this.spawnObstacle();
-  this.animationFrameId = requestAnimationFrame(() => this.animate());
-};
+// Game.prototype.animate = function() {
+//   this.ctx.clearRect(0, 0, this.width, this.height);
+//   this.player.update();
+//   this.player.draw();
 
-Game.prototype.drawBackground = function() {
-  const bg = new Image();
-  bg.src = 'assets/images/background_single.png';
-  this.ctx.drawImage(bg, 0, 0, this.width, this.height);
-};
+//   this.obstacles.forEach((obstacle, index) => {
+//       obstacle.update();
+//       obstacle.draw();
+//       if (this.player.collidesWith(obstacle)) {
+//           cancelAnimationFrame(this.animationFrameId);
+//           alert('Game Over');
+//       }
+//   });
+//   this.spawnObstacle();
+//   this.animationFrameId = requestAnimationFrame(() => this.animate());
+// };
 
-Game.prototype.animate = function() {
-  this.ctx.clearRect(0, 0, this.width, this.height);
-  this.drawBackground();
-  this.player.update();
-  this.player.draw();
-  this.obstacles.forEach(obstacle => {
-      obstacle.update();
-      obstacle.draw();
-  });
-  this.spawnObstacle();
-  this.animationFrameId = requestAnimationFrame(() => this.animate());
-};
+// Game.prototype.drawBackground = function() {
+//   const bg = new Image();
+//   bg.src = 'assets/images/background_single.png';
+//   this.ctx.drawImage(bg, 0, 0, this.width, this.height);
+// };
+
+// Game.prototype.animate = function() {
+//   this.ctx.clearRect(0, 0, this.width, this.height);
+//   this.drawBackground();
+//   this.player.update();
+//   this.player.draw();
+//   this.obstacles.forEach(obstacle => {
+//       obstacle.update();
+//       obstacle.draw();
+//   });
+//   this.spawnObstacle();
+//   this.animationFrameId = requestAnimationFrame(() => this.animate());
+// };
--- scripts/player.js
+++ scripts/player.js
@@ -2,54 +2,62 @@ class Player {
   constructor (game) {
     this.game = game;
     this.width = 50;
-    this.height = 50;
-    this.x = 100;
-    this.y = this.game.height / 2 - this.height / 2;
+    this.height = 60;
+    this.spriteWidth = 200;
+    this.spriteHeight = 200;
+    this.width = 200;
+    this.height = 200;
   }
 
   update() {
-    this.speedY += this.game.gravity;
-    this.y += this.speedY;
+    this.x++;
+  //   this.speedY += this.game.gravity;
+  //   this.y += this.speedY;
 
-    if (this.y + this.height > this.game.height) {
-      this.y = this.game.height - this.height;
-      this.speedY = 0;
-    }
+  //   if (this.y + this.height > this.game.height) {
+  //     this.y = this.game.height - this.height;
+  //     this.speedY = 0;
+  //   }
 
-    if (this.y < 0) {
-      this.y = 0;
-      this.speedY = 0;
-    }
+  //   if (this.y < 0) {
+  //     this.y = 0;
+  //     this.speedY = 0;
+  //   }
   }
 
   draw() {
     this.game.ctx.fillStyle = 'red';
     this.game.ctx.fillRect(this.x, this.y, this.width, this.height);
   }
-}
 
-document.addEventListener('keydown', (e) => {
-  if (e.keyCode === 'space') {
-    game.player.jump();
+  resize() {
+    this.width = this.spriteWidth * this.game.ratio;
+    this.height = this.spriteHeight * this.game.ratio;
   }
-});
-
-Player.prototype.jump = function() {
-  this.speedY = -10;
 }
 
-Player.prototype.collidesWith = function(obstacle) {
-  return !(
-      this.x + this.width < obstacle.x ||
-      this.x > obstacle.x + obstacle.width ||
-      this.y + this.height < obstacle.y ||
-      this.y > obstacle.y + obstacle.height
-  );
-};
+// document.addEventListener('keydown', (e) => {
+//   if (e.keyCode === 'space') {
+//     game.player.jump();
+//   }
+// });
 
-Player.prototype.draw = function() {
-  const img = new Image();
-  img.src = 'assets/images/player_fish.png';
-  this.game.ctx.drawImage(img, this.x, this.y, this.width, this.height);
-};
+// Player.prototype.jump = function() {
+//   this.speedY = -10;
+// }
+
+// Player.prototype.collidesWith = function(obstacle) {
+//   return !(
+//       this.x + this.width < obstacle.x ||
+//       this.x > obstacle.x + obstacle.width ||
+//       this.y + this.height < obstacle.y ||
+//       this.y > obstacle.y + obstacle.height
+//   );
+// };
+
+// Player.prototype.draw = function() {
+//   const img = new Image();
+//   img.src = 'assets/images/player_fish.png';
+//   this.game.ctx.drawImage(img, this.x, this.y, this.width, this.height);
+// };
 
